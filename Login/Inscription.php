d8f2535345c902e35fa0f7fff597a0a8081d20e9
FrancoisSLB <44406281+FrancoisSLB@users.noreply.github.com>
2018-11-21 09:59:39 +0100

    Create Inscription.php

---
+++ Login/Inscription.php
@@ -0,0 +1,67 @@
+html {
+
+<?php
+    $nom = "";
+    $adresse = "";
+    $datenaissance = "";
+    $pays = "";
+    $codepostal = "";
+    $email = "";
+    $genre = "";
+    $commentaire = ""
+    $website = "";
+
+    $nomErreur = "";
+    $adresseErreur = "";
+    $datenaissanceErreur = "";
+    $paysErreur = "";
+    $codepostalErreur = "";
+    $emailErreur = "" ;
+    $genreErreur = "";
+    $websiteErreur = "";
+
+  
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {                                     //Vérifie que le serveur demande des données (POST)
+
+    if (empty($_POST["nom"])) {                                                  //Cas où la case nom n'est pas remplie
+      $nomErreur = "Le nom est obligatoire";
+    } else {
+      $nom = test_input($_POST["nom"]);                                          
+
+      if (!preg_match("/ ^ [a-zA-Z ] * $ /",$nom)) {                                  // Vérifie que le nom ne comporte que des lettres et des espaces
+        $nomErreur = "Seulement des lettres et des espaces sont acceptés";
+      }
+    }
+    
+    if (empty($_POST["email"])) {
+      $emailErreur = "Le mail est obligatoire";
+    } else {
+
+      $email = test_input($_POST["email"]);                                      
+
+      if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {                           // Vérifie que le format mail est valide
+        $emailErreur = "Le format mail est invalide";
+       }
+    }
+
+    if (empty($_POST[ "commentaire" ])) {
+      $commentaire = "";
+    } else {
+      $commentaire = test_input($_POST[ "commentaire" ]);
+    }
+  
+    if (empty($_POST[ "genre" ])) {
+      $genreErreur = "Le genre est obligatoire";
+    } else {
+      $genre = test_input($_POST[ "genre" ]);
+    }
+  }
+  function test_input($data) {
+    $data = trim(data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+  }
+  
+  ?>
+}
