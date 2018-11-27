1a036b217fad8e6de305af7f11a7728f013a29d5
FrancoisSLB <44406281+FrancoisSLB@users.noreply.github.com>
2018-11-27 17:20:04 +0100

    Update Inscription.php

--- Login/Inscription.php
+++ Login/Inscription.php
@@ -3,16 +3,18 @@ html
 <?php
     $nom = "";
     $adresse = "";
+    $numerotelephone = "";
     $datenaissance = "";
     $pays = "";
     $codepostal = "";
     $email = "";
     $genre = "";
-    $commentaire = ""
+    $commentaire = "";
     $website = "";
 
     $nomErreur = "";
     $adresseErreur = "";
+    $numerotelephoneErreur = "";
     $datenaissanceErreur = "";
     $paysErreur = "";
     $codepostalErreur = "";
@@ -21,26 +23,69 @@ html
     $websiteErreur = "";
 
   
-  if ($_SERVER["REQUEST_METHOD"] == "POST") {                                     //Vérifie que le serveur demande des données (POST)
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {                                     
+      //Vérifie que le serveur demande des données (POST)
 
-    if (empty($_POST["nom"])) {                                                  //Cas où la case nom n'est pas remplie
+    if (empty($_POST["nom"])) {    
+      //Cas où la case nom n'est pas remplie
       $nomErreur = "Le nom est obligatoire";
+        
     } else {
+        
       $nom = test_input($_POST["nom"]);                                          
 
-      if (!preg_match("/ ^ [a-zA-Z ] * $ /",$nom)) {                                  // Vérifie que le nom ne comporte que des lettres et des espaces
+      if (!preg_match("/ ^ [a-zA-Z ] * $ /",$nom)) {     
+        // Vérifie que le nom ne comporte que des lettres et des espaces
         $nomErreur = "Seulement des lettres et des espaces sont acceptés";
+          
       }
     }
     
+    if (empty($_POST["numerotelephone"])) {    
+      //Cas où la case numerotelephone n'est pas remplie
+      $numerotelephoneErreur = "Le numéro de téléphone est obligatoire";
+        
+    } else {
+      
+      $numerotelephone = test_input($_POST["numerotelephone"]);                                          
+
+      if ($numerotelephone>9999999999) {     
+        // Vérifie que le numero de téléphone comporte 12 nombres
+        $numerotelephoneErreur = "Numéro de téléphone incorrecte";
+          
+      }
+    }
+     
+    if (empty($_POST[ "datenaissance" ])) {
+      $datenaissance = "";
+        } else {
+      $datenaissance = test_input($_POST[ "datenaissance" ]);
+     }
+     
+    if (empty($_POST[ "pays" ])) {
+      $pays = "";
+        } else {
+      $pays = test_input($_POST[ "pays" ]);
+     }
+    
+    if (empty($_POST[ "codepostal" ])) {
+      $codepostal = "";
+        } else {
+      $codepostal = test_input($_POST[ "codepostal" ]);
+     }
+      
     if (empty($_POST["email"])) {
+      //Cas où la case email n'est pas remplie  
       $emailErreur = "Le mail est obligatoire";
+        
     } else {
 
       $email = test_input($_POST["email"]);                                      
 
-      if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {                           // Vérifie que le format mail est valide
+      if (!filter_var($email, FILTER_VALIDATE_EMAIL)) { 
+        // Vérifie que le format mail est valide
         $emailErreur = "Le format mail est invalide";
+          
        }
     }
 
@@ -63,9 +108,9 @@ html
     return $data;
   }
   
-  if (isset($genre) && $genre == "femme" ) echo "checked" ;
-  if (isset($genre) && $genre == "homme" ) echo "checked" ;
-  if (isset($genre) && $genre == "autre" ) echo "checked" ;
+  if (isset($genre) && $genre == "femme" ) echo "femme cochée" ;
+  if (isset($genre) && $genre == "homme" ) echo "homme coché" ;
+  if (isset($genre) && $genre == "autre" ) echo "autre coché" ;
   
   echo $nom ;
   echo $adresse ;
