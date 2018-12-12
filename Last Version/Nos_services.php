3dbc23fa0d227c8a1f9ae268e419671104fcf04c
ISEP-Pro2 <44368702+ISEP-Pro2@users.noreply.github.com>
2018-12-12 09:31:56 +0100

    Add files via upload

---
+++ Last Version/Nos_services.php
@@ -0,0 +1,71 @@
+<!DOCTYPE HTML>
+
+<html>
+	<head>
+		<title>Nos Services</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="services.css" />
+	</head>
+	<body class="homepage is-preload">
+		<div id="page-wrapper">
+
+				<section id="header" >
+				        <a href="Accueil.php"><img id="logo" src="Images/logo_provisoire2.png"> </img></a>
+
+						<h1><a href="index.html">Nos services</a></h1>
+
+				</section>
+
+			<!-- Main -->
+				<section id="main">
+					<div class="container">
+						<div class="row">
+							<div class="col-12">
+
+								<!-- Portfolio -->
+									<section>
+										<header class="major">
+											<h2>Veuillez sélectionner votre type de logement :</h2>
+										</header>
+										<div class="row">
+											<div class="col-4 col-6-medium col-12-small">
+												<section class="box">
+													<a href="#" class="image featured"><img src="Images/appart.jpg" alt="" /></a>
+													<header>
+														<h3>Appartement</h3>
+													</header
+													<footer>
+														<ul class="actions">
+															<li><a href="services_appart.php" class="button alt">En savoir plus</a></li>
+														</ul>
+													</footer>
+												</section>
+											</div>
+											<div class="col-4 col-6-medium col-12-small">
+												<section class="box">
+													<a href="#" class="image featured"><img src="Images/maison.jpg" alt="" /></a>
+													<header>
+														<h3>Maison</h3>
+													</header
+													<footer>
+														<ul class="actions">
+															<li><a href="services_maison.php" class="button alt">En savoir plus</a></li>
+														</ul>
+													</footer>
+												</section>
+											</div>
+											
+										</div>
+									</section>
+
+							</div>
+							
+						</div>
+					</div>
+				</section>
+
+			
+
+	</body>
+</html>
---
+++ Last Version/services_appart.php
@@ -0,0 +1,40 @@
+<!DOCTYPE HTML>
+
+	<head>
+		<title>Appart</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="services.css" />
+	</head>
+	<body class="no-sidebar is-preload">
+		<div id="page-wrapper">
+
+			<!-- Header -->
+				<section id="header">
+
+					<!-- Logo -->
+						<a href="Accueil.php"><img id="logo" src="Images/logo_provisoire2.png"> </img></a>
+						<h1><a href="index.html">Appartement</a></h1>
+
+				</section>
+
+			<!-- Main -->
+				<section id="main">
+					<div class="container">
+
+						<!-- Content -->
+							<article class="box post">
+								<a href="#" class="image featured"><img src="images/pic01.jpg" alt="" /></a>
+								<header>
+									<h2>Notre formule appartement:</h2>
+								</header>
+								<p> Image de la formule
+								</p>
+							</article>
+
+					</div>
+				</section>
+
+			
+	</body>
+</html>
