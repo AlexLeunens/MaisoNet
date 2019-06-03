252fdf4609f7576e164708d188b39513ae915b2c
AlexLeunens <alexandre.leunens@gmail.com>
2019-06-03 14:45:13 +0200

    correction d'un mauvais push sur ces fichiers :(

--- maisonetMVC/maisonet/views/user/user.php
+++ maisonetMVC/maisonet/views/user/user.php
@@ -1,47 +1,34 @@
 <?php
-
 //TODO implementer l'ajout de maison, de pièces et de capteurs
-
-
 $title = "User Interface";
 $css = "/maisonet/views/user/utilisateur1.css";
 require ROOT . "/views/template/headerMainUser.php";
 include ROOT . "/models/connect.php";
 include_once ROOT . "/models/secure.php";
-
 if (session_status() !== PHP_SESSION_ACTIVE) {
     session_start();
-
 }
 // DEBUG
 $_SESSION["name"] = "Anonym";
 $_SESSION["firstname"] = "Name";
 $_SESSION['type'] = 2;
-
 if (isset($_SESSION["adresse"])) {
     $adresse = $_SESSION["adresse"];
 }
-
-
 // Get user id
 $sql = "SELECT idUtilisateur FROM utilisateur WHERE utilisateur.Nom = '" . $_SESSION["name"] . "' AND utilisateur.prenom = '" . $_SESSION["firstname"] . "'";
 $result = $conn->query($sql);
-
 while ($row = $result->fetch_assoc()) {
     $_SESSION['idUser'] = $row["idUtilisateur"];
 }
-
 ?>
 
 
 <?php
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-
     if (isset($_POST['getMaison'])) {
-
         $adresse = str_replace(" ", "", $_POST['adresse']);
         $_SESSION["adresse"] = $adresse;
-
     }
 }
 ?>
@@ -82,7 +69,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $sql = "SELECT * FROM maison WHERE Utilisateur_idUtilisateur =" . $_SESSION['idUser'] . "";
         $result = $conn->query($sql);
         $incrementDropdown = 0;
-
         echo "<select name='adresse'>";
         while ($maisonsUser = $result->fetch_assoc()) {
             $incrementDropdown++;
@@ -125,46 +111,17 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 </div>
 
 
-<p class="addHome" id="addHome"> + </p>
+<p class="addHome"> + </p>
 <div class="addHomePanel">
 
-    <button onclick="showDialog(this.nextElementSibling)">Ajouter une Maison</button>
-    <div class="dialog" id="dialog" style="display: none">
-        <h1>Hello</h1>
-    </div>
+    <a href="" onclick="tabFAQ()"><img class="imageshelp" src="/maisonet/views/user/Images-utilisateur/help.png" alt="FAQ"></img></a>
 
-
-    <button>Ajouter une Piece</button>
-    <button>Ajouter un Capteur</button>
+    <a href="" onclick="popupContact()"><img class="imageshelp" src="/maisonet/views/user/Images-utilisateur/helptechnician.png" alt="Contact Tech"></img> </a>
 
 </div>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
 <script>
-
-    /*
-    var addHome = document.getElementsByClassName("addHome")
-    var addHomePanel = document.getElementsByClassName("addHomePanel")
-    addHome[0].onclick = function () {
-        var setClasses = !this.classList.contains('active'); // vérifie si help actif
-        setClass(addHome, 'active', 'remove'); //les rend inactives
-        setClass(addHomePanel, 'show', 'remove'); // cache le contenu
-        if (setClasses) { //si help pas deja actif
-            this.classList.toggle("active");
-            this.nextElementSibling.classList.toggle("show");
-        }
-    }
-    */
-
-    $("#addHome").addEventListener('click', function() {
-        this.nextElementSibling.classList.toggle("show");
-    }, false);
-
-    function showDialog(dialog) {
-        dialog.style.display="block";
-    }
-
-
     var piece = document.getElementsByClassName("piece");
     var panel = document.getElementsByClassName('panel'); //selec piece et panel
     for (var i = 0; i < piece.length; i++) { //pour tout bouton
@@ -178,15 +135,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             }
         }
     }
-
     $(document).on("click", "p.piece", function () {
         var a = !this.classList.contains("active");
         setClass(piece, "active", "remove");
         setClass(panel, "show", "remove");
         a && (this.classList.toggle("active"), this.nextElementSibling.classList.toggle("show"));
     });
-
-
     function toggle_visibility(id) {
         var e = document.getElementById(id);
         if (e.style.display == 'block')
@@ -194,32 +148,47 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         else
             e.style.display = 'block';
     }
-
+    var addHome = document.getElementsByClassName("addHome")
+    var addHomePanel = document.getElementsByClassName("addHomePanel")
+    addHome[0].onclick = function () {
+        var setClasses = !this.classList.contains('active'); // vérifie si help actif
+        setClass(addHome, 'active', 'remove'); //les rend inactives
+        setClass(addHomePanel, 'show', 'remove'); // cache le contenu
+        if (setClasses) { //si help pas deja actif
+            this.classList.toggle("active");
+            this.nextElementSibling.classList.toggle("show");
+        }
+    }
     function setClass(els, className, fnName) {
         for (var i = 0; i < els.length; i++) { //chaque piece selec avec !this.classList.contains('active')
             els[i].classList[fnName](className); //les prend une par une, puis désactive une propriété
         }
     }
-
     function openNav() {
         document.getElementById("mySidenav").style.width = "250px";
     }
-
     function closeNav() {
         document.getElementById("mySidenav").style.width = "0";
     }
-
+    function popupHelp() {
+        var myWindow = window.open("views/FAQ/FAQ.php", "", "width=1200, height=1000");
+    }
+    function popupContact() {
+        var myWindow = window.open("contact.html", "", "width=800, height=500, left=500px, top=200px");
+    }
+    function tabFAQ() {
+        var win = window.open("views/FAQ/FAQ.php", '_blank');
+        win.focus();
+    }
     function switchClick() {
         document.getElementById("switchPresence").click();
     }
-
     function openPage(elmnt) {
         var i, tabcontent, pageName, ongletActif;
         var checkBox = document.getElementById("switchPresence");
         var ongletPresent = document.getElementById("liPresent");
         var ongletAbsent = document.getElementById("liAbsent");
         tabcontent = document.getElementsByClassName("fonctions");
-
         if (checkBox.checked == true) {
             pageName = "Present";
             ongletAbsent.classList.remove("active"); /*update les onglets*/
@@ -234,17 +203,14 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         }
         document.getElementById(pageName).style.display = "grid"; //disp selected
     }
-
     if (document.getElementById("switchPresence").checked == false) {
         document.getElementById("switchPresence").click(); // Update le switch lors d'un rafraichissement
     }
-
     <?php
     if (isset($adresse)) {
         echo "$('#maisonAffiche').load('afficheMaison.php').fadeIn('slow');";
     }
     ?>
-
 </script>
 
 
