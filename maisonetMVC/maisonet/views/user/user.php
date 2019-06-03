bb605eed720b9ff49521292a3030a41ccc59cd16
AlexLeunens <alexandre.leunens@gmail.com>
2019-06-03 16:09:23 +0200

    addition d'un bouton d'ajout de capteur sur l'interface utilisateur. TODO : requete pour l'insérer

--- maisonetMVC/maisonet/views/user/user.php
+++ maisonetMVC/maisonet/views/user/user.php
@@ -66,7 +66,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <form class='dossier' method='post' action=''>
         <label for="text">Choississez votre maison :</label>
         <?php
-        $sql = "SELECT * FROM maison WHERE Utilisateur_idUtilisateur =" . $_SESSION['idUser'] . "";
+        $sql = "SELECT Nom FROM piece WHERE Maison_idMaison =" . $_SESSION['idUser'] . "";
         $result = $conn->query($sql);
         $incrementDropdown = 0;
         echo "<select name='adresse'>";
@@ -76,9 +76,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         }
         echo "</select>";
         ?>
-        <input align="right" type="submit" name="getMaison" value="Entrée">
+        <input align="right" type="submit" name="submitCapteur" value="Entrée">
     </form>
-
+    // TODO action pour insérer le nouveau capteur dans la base de donnée
     <div id="maisonAffiche"></div>
 
 </div>
@@ -91,10 +91,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             <img class="imagesbutton" src="views/user/Images-utilisateur/temperature.png" alt="temperature"></img>
             <p>Valeur température</p>
         </div>
-        <div class="block">
-            <img class="imagesbutton" src="views/user/Images-utilisateur/volet.png" alt="volets"></img>
-            <p> Etat des volets</p>
-        </div>
     </div>
 
     <p class="piece">Sécurité</p>
@@ -102,10 +98,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         <div class="block">
             <img class="imagesbutton" src="/maisonet/views/user/Images-utilisateur/temperature+.png" alt="temperature"></img>
             <p>Valeur température</p>
-        </div>
-        <div class="block">
-            <img class="imagesbutton" src="/maisonet/views/user/Images-utilisateur/volets2.png" alt="volets"></img>
-            <p> Etat des volets</p>
         </div>
     </div>
 </div>
@@ -118,10 +110,89 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     <a href="" onclick="popupContact()"><img class="imageshelp" src="/maisonet/views/user/Images-utilisateur/helptechnician.png" alt="Contact Tech"></img> </a>
 
+    <button onclick="ajouterCapteur()" id="addCapteur"> Ajouter un Capteur</button>
+
+</div>
+
+<div id="ajoutCapteur" class="modal">
+
+    <div class="modal-content">
+        <span class="close">&times;</span>
+
+        <form class="addCapteurType" method="post" action="">
+            <h3>Ajouter un Nouveau Capteur</h3>
+
+            <label for="inputCapteurType">Type</label>
+            <?php
+            $sql = "SELECT DISTINCT Type FROM capteur ";
+            $result = $conn->query($sql);
+            $incrementDropdown = 0;
+            echo "<select name='type' class='editableBox'>";
+            echo "<option value=''></option>";
+            while ($capteurs = $result->fetch_assoc()) {
+                $incrementDropdown++;
+                echo "<option value='" . $capteurs["Type"] . "'>" . $capteurs["Type"] . "</option>";
+            }
+            echo "</select>";
+            ?>
+            <input class="timeTextBox" name="timebox" maxlength="20"/>
+            <br>
+
+            <label for="inputCapteurPiece">Pièce</label>
+            <?php
+            $sql = "SELECT nom FROM piece WHERE Maison_idMaison = ( SELECT idMaison FROM maison WHERE maison.Adresse = '".$_SESSION['adresse']."')";
+            $result = $conn->query($sql);
+            $incrementDropdown = 0;
+            echo "<select name='piece'>";
+
+            while ($pieces = $result->fetch_assoc()) {
+                $incrementDropdown++;
+                echo "<option value='" . $pieces["nom"] . "'>" . $pieces["nom"] . "</option>";
+            }
+            echo "</select>";
+            ?>
+
+            <br>
+
+
+            <input type="submit" value="Ajouter">
+
+        </form>
+
+    </div>
+
 </div>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
 <script>
+
+    var modal = document.getElementById("ajoutCapteur");
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("addCapteur");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+
+    // When the user clicks on the button, open the modal
+    btn.onclick = function () {
+        modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+
+
     var piece = document.getElementsByClassName("piece");
     var panel = document.getElementsByClassName('panel'); //selec piece et panel
     for (var i = 0; i < piece.length; i++) { //pour tout bouton
@@ -141,6 +212,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         setClass(panel, "show", "remove");
         a && (this.classList.toggle("active"), this.nextElementSibling.classList.toggle("show"));
     });
+
     function toggle_visibility(id) {
         var e = document.getElementById(id);
         if (e.style.display == 'block')
@@ -148,6 +220,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         else
             e.style.display = 'block';
     }
+
     var addHome = document.getElementsByClassName("addHome")
     var addHomePanel = document.getElementsByClassName("addHomePanel")
     addHome[0].onclick = function () {
@@ -159,30 +232,38 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             this.nextElementSibling.classList.toggle("show");
         }
     }
+
     function setClass(els, className, fnName) {
         for (var i = 0; i < els.length; i++) { //chaque piece selec avec !this.classList.contains('active')
             els[i].classList[fnName](className); //les prend une par une, puis désactive une propriété
         }
     }
+
     function openNav() {
         document.getElementById("mySidenav").style.width = "250px";
     }
+
     function closeNav() {
         document.getElementById("mySidenav").style.width = "0";
     }
+
     function popupHelp() {
         var myWindow = window.open("views/FAQ/FAQ.php", "", "width=1200, height=1000");
     }
+
     function popupContact() {
         var myWindow = window.open("contact.html", "", "width=800, height=500, left=500px, top=200px");
     }
+
     function tabFAQ() {
         var win = window.open("views/FAQ/FAQ.php", '_blank');
         win.focus();
     }
+
     function switchClick() {
         document.getElementById("switchPresence").click();
     }
+
     function openPage(elmnt) {
         var i, tabcontent, pageName, ongletActif;
         var checkBox = document.getElementById("switchPresence");
@@ -203,6 +284,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         }
         document.getElementById(pageName).style.display = "grid"; //disp selected
     }
+
     if (document.getElementById("switchPresence").checked == false) {
         document.getElementById("switchPresence").click(); // Update le switch lors d'un rafraichissement
     }
@@ -211,6 +293,13 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         echo "$('#maisonAffiche').load('afficheMaison.php').fadeIn('slow');";
     }
     ?>
+
+    $(document).ready(function(){
+
+        $(".editableBox").change(function(){
+            $(".timeTextBox").val($(".editableBox option:selected").html());
+        });
+    });
 </script>
 
 
